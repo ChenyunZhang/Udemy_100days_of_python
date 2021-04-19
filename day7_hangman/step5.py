0fe76e529b2e232b63035ae40b703997cc19cfd4
Chenyun Zhang <morningchenyun@gmail.com>
2021-04-19 12:03:54 -0400

    finsihed hangman

---
+++ day7_hangman/step5.py
@@ -0,0 +1,32 @@
+import random
+from words import word_list
+from hangman_art import stages
+from hangman_art import logo
+
+
+def hangman():
+    print(logo)
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
+    end_of_game = False
+    lives = 6
+    display = ["_" for _ in range(word_length)]
+    while not end_of_game:
+        guess = input("Guess a letter: ").lower()
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                print("Good guess! :)")
+                display[position] = letter
+        if guess not in chosen_word:
+            print("Nice try, but nope :(")
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print(f"You lose. The chosen word is {chosen_word}.")
+        if "_" not in display:
+            end_of_game = True
+            print(f"You win. It's {chosen_word},")
+        print(stages[lives])
+        print(f"{' '.join(display)}")
+hangman()
